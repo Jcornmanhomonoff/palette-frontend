e672b1356af7897119e8835aa437979221c7868d
jcornmanhomonoff <jessica.cornman-homonoff@generalassemb.ly>
2018-04-16 14:01:02 -0400

    Add event handlers
    - Remove semicolons

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,5 +7,58 @@
 // require('./example')
 
 $(() => {
-  // your JS code goes here
+  require('./example')
+  require('./jribbble')
+
+  const getFormFields = require('../../lib/get-form-fields')
+
+  const app = require('./app-data.js')
+  const authApi = require('./auth/api')
+  const authUi = require('./auth/ui')
+  const authFavorites = require('./auth/favoritesApi')
+  // const app = require('./app-data.js')
+
+  // const addHandlers = () => {
+  $('#sign-up').on('submit', function (event) {
+    let data = getFormFields(this)
+    console.log("sign-up success")
+    event.preventDefault()
+    authApi.signUp(authUi.success, authUi.failure, data)
+  })
+  $('#sign-in').on('submit', function (event) {
+    let data = getFormFields(this)
+    console.log("sign-in success")
+    event.preventDefault()
+    authApi.signIn(authUi.signInSuccess, authUi.failure, data)
+  })
+  $('#sign-out').on('click', function (event) {
+    console.log("sign-out success")
+    event.preventDefault()
+    authApi.signOut(authUi.signOutSuccess, authUi.failure)
+  })
+  $('#change-password').on('submit', function (event) {
+    let data = getFormFields(this)
+    event.preventDefault()
+    authApi.changePassword(authUi.changePasswordSuccess, authUi.failure, data)
+  })
+
+  // SIGN UP AND IN, MODAL DROP DOWN
+
+  // OPENS SIGNIN FROM DROPDOWN
+  $('.open-sign-in').on('click', function (event) {
+    event.preventDefault()
+    $('#signInModal').modal('show')
+  })
+
+  // OPENS SIGNUP FROM DROPDOWN
+  $('.open-sign-up').on('click', function (event) {
+    event.preventDefault()
+    $('#signUpModal').modal('show')
+  })
+
+  // OPENS CHANGE PASSWORD FROM DROPDOWN
+  $('.open-change-password').on('click', function (event) {
+    event.preventDefault()
+    $('#changePasswordModal').modal('show')
+  })
 })
