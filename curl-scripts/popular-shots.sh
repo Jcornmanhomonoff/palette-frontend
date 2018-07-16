189fb92bd1889626caa5bb81bb4cf50156396304
jcornmanhomonoff <jessica.cornman-homonoff@generalassemb.ly>
2018-07-16 11:55:21 -0400

    Add curl script for popular shots

--- curl-scripts/popular-shots.sh
+++ curl-scripts/popular-shots.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-curl "https://api.dribbble.com/v2/popular_shots" \
-  --include \
-  --request GET \
-  --header "Authorization: Bearer 14bf2a248a42fcf7989fe3cbb10fc5377291102486a976e6efd54a22462ba549"
+curl "https://dribbble.com/oauth/authorize" \
+-- include \
+-- request GET
 
 echo
